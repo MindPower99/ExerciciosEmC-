bb855940f335364e71cdb54608def2acca4d6b01
MindPower99 <gabriel-jorge1999@hotmail.com>
2018-09-20 20:35:47 -0300

    Exercicio de soma de dois numeros

---
+++ ExercicioSoma/ExercicioSoma/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace eae
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbn1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbn2 = new System.Windows.Forms.TextBox();
+            this.btnsoma = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbresult = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txbn1
+            // 
+            this.txbn1.Location = new System.Drawing.Point(119, 48);
+            this.txbn1.Name = "txbn1";
+            this.txbn1.Size = new System.Drawing.Size(100, 20);
+            this.txbn1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero 2";
+            // 
+            // txbn2
+            // 
+            this.txbn2.Location = new System.Drawing.Point(265, 48);
+            this.txbn2.Name = "txbn2";
+            this.txbn2.Size = new System.Drawing.Size(100, 20);
+            this.txbn2.TabIndex = 3;
+            // 
+            // btnsoma
+            // 
+            this.btnsoma.Location = new System.Drawing.Point(264, 105);
+            this.btnsoma.Name = "btnsoma";
+            this.btnsoma.Size = new System.Drawing.Size(75, 23);
+            this.btnsoma.TabIndex = 4;
+            this.btnsoma.Text = "somar";
+            this.btnsoma.UseVisualStyleBackColor = true;
+            this.btnsoma.Click += new System.EventHandler(this.btnsoma_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "=";
+            // 
+            // txbresult
+            // 
+            this.txbresult.Location = new System.Drawing.Point(421, 48);
+            this.txbresult.Name = "txbresult";
+            this.txbresult.Size = new System.Drawing.Size(100, 20);
+            this.txbresult.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(445, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 140);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbresult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnsoma);
+            this.Controls.Add(this.txbn2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbn1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbn1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbn2;
+        private System.Windows.Forms.Button btnsoma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbresult;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ExercicioSoma/ExercicioSoma/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace eae
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsoma_Click(object sender, EventArgs e)
+        {
+            int n1, n2;
+            n1 = Convert.ToInt32(txbn1.Text);
+            n2 = Convert.ToInt32(txbn2.Text);
+            int soma = n1 + n2;
+            txbresult.Text = soma.ToString();
+        }
+
+
+    }
+}
